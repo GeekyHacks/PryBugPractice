16d61b134ca5b0ba042a5fa3c9432bc36040cb04
Abdullah <geekyhacks22@gmail.com>
2023-10-21 21:10:22 +0300

    Add the ruby files with a bug

---
+++ person.rb
@@ -0,0 +1,37 @@
+require './nameable'
+require './trimmerdecorator'
+require './capitalizedecorator'
+require './rental'
+require './book'
+
+class Person < Nameable
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
+  def initialize(age, name, parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
